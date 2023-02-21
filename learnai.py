6ffcef0ad16223c448e6d506610be91f8fee7a64
ymoola <ymoola21@gmail.com>
2023-02-21 16:50:46 -0500

    files

--- learnai.py
+++ learnai.py
@@ -5,3 +5,9 @@ import cv2
 import os
 import glob
 import matplotlib.pyplot as plt
+import kaggle
+import opendatasets 
+
+#Importing data set from Kaggle
+os.environ['KAGGLE_USERNAME'] = 'yusufmoola211'
+os.environ['KAGGLE_KEY'] = '00c7ae4c784f63a68821345cd24f9474'
